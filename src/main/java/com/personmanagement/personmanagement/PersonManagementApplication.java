4590f8f23039a507454a5833b2f5ae6280dd6475
Kasim Abubakar Jajere <alkasima@gmail.com>
2023-09-12 10:37:36 +0100

    Push code to github

--- src/main/java/com/personmanagement/personmanagement/PersonManagementApplication.java
+++ src/main/java/com/personmanagement/personmanagement/PersonManagementApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonManagementApplication {
+public class PersonManagementApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonManagementApplication.class, args);
